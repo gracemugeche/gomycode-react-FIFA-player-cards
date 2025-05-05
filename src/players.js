fed6ce6df279e0aae0469be8a03b5d54bac53b3c
gracemugeche <gracemugeche@gmail.com>
2025-05-05 13:03:21 +0300

    Add players data array

---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Miami",
+      nationality: "Argentina",
+      jerseyNumber: 10,
+      age: 36,
+      image: "/images/messi.jpg"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al Nassr",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 39,
+      image: "/images/ronaldo.jpg"
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 24,
+      image: "/images/mbappe.jpg"
+    },
+    {
+      name: "Neymar Jr.",
+      team: "Al Hilal",
+      nationality: "Brazil",
+      jerseyNumber: 11,
+      age: 31,
+      image: "/images/neymar.jpg"
+    }
+  ];
+  
+  export default players;
+  
