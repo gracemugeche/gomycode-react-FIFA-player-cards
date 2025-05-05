31011784ae79d50c33b64460cb240faa571c3b9c
gracemugeche <gracemugeche@gmail.com>
2025-05-05 14:36:05 +0300

    create playerlist component that maps and displays player cards

--- src/PlayersList.jsx
+++ src/PlayersList.jsx
@@ -1,8 +1,10 @@
+// Import React and required components
 import React from "react";
 import Player from "./Player";
 import players from "./players";
 import { Container, Row, Col } from "react-bootstrap";
 
+// PlayersList component will display all players in a responsive grid
 const PlayersList = () => {
   return (
     <Container>
